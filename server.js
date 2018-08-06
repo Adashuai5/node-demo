a9061c3bc089a5c550987c9209e25dffbf4f5024
adashuai5 <ad873606339@gmail.com>
2018-08-06 23:16:28 +0800

    添加 db 数据库 用form发POST请求

--- server.js
+++ server.js
@@ -10,51 +10,43 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
 
-
-
-
-
-console.log('HTTP 路径为\n' + path)
-  if(path == '/style.css'){
-    var string = fs.readFileSync('./style.css','utf8')
+  if (path === '/style.css') {
+    var string = fs.readFileSync('./style.css', 'utf8')
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path == '/main.js'){
-    var string = fs.readFileSync('./main.js','utf8')
+  } else if (path === '/main.js') {
+    var string = fs.readFileSync('./main.js', 'utf8')
     response.setHeader('Content-Type', 'application/javascript; charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path == '/'){
-    var string = fs.readFileSync('./index.html','utf8')
+  } else if (path === '/') {
+    var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8')
+    string = string.replace('&&&amount&&&', amount)
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write(string)
     response.end()
-  }else{
+  } else if (path === '/pay' && method.toUpperCase() === 'POST') {
+    var amount = fs.readFileSync('./bd', 'utf8')
+    var newAmount = amount - 1
+    fs.writeFileSync('./db', newAmount)
+    response.write('success')
+    response.end()
+  } else {
     response.statusCode = 404
     response.end()
-  }  
-
-
-
-
-
-
-
-
-
-
-
+  }
 
 
 
@@ -65,5 +57,3 @@ console.log('HTTP 路径为\n' + path)
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-

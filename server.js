1ac7554a80c3c26243ae60184d1afaa3bc104b25
adashuai5 <ad873606339@gmail.com>
2018-08-07 21:10:33 +0800

    SRJ 无刷新更新页面内容

--- server.js
+++ server.js
@@ -34,9 +34,10 @@ var server = http.createServer(function (request, response) {
     var newAmount = amount - 1
     if (Math.random() > 0.5) {
       fs.writeFileSync('./db', newAmount)
-      response.setHeader('Content-Type','image/png')
+      response.setHeader('Content-Type', 'application/javascript')
       response.statusCode = 200
-      response.write(fs.readFileSync('./dog.jpg'))
+      response.write(`alert("success")
+      amount.innerText = amount.innerText-1`)
     } else {
       response.statusCode = 400
       response.write('fail')

6410176bc0c0561e42eab415f797a4e032c4ad7d
adashuai5 <ad873606339@gmail.com>
2018-08-07 20:01:58 +0800

    img发请求且不刷新页面

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ var server = http.createServer(function (request, response) {
     var newAmount = amount - 1
     if (Math.random() > 0.5) {
       fs.writeFileSync('./db', newAmount)
+      response.setHeader('Content-Type','image/png')
       response.statusCode = 200
       response.write(fs.readFileSync('./dog.jpg'))
     } else {

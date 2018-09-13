f91207c98a6f23131e29a4844f7698133a411871
adashuai5 <ad873606339@gmail.com>
2018-09-13 22:16:01 +0800

    登录界面完善

--- cookie-demo/server.js
+++ cookie-demo/server.js
@@ -88,8 +88,60 @@ var server = http.createServer(function (request, response) {
             }
             response.end()
         })
-    }
-    //注意这里是 /main.js 而不是 ./main.js，因为HTTP请求永远是绝对路径
+    }else if (path === '/sign_in' && method === 'GET') {
+        let string = fs.readFileSync('./sign_in.html', 'utf8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html; charset=utf-8')
+        response.write(string)
+        response.end()
+    } else if (path === '/sign_in' && method === 'POST') {
+        readBody(request).then((body) => {
+            let strings = body.split('&') // ['email=11','password=12','password_confirmation=2']
+            let hash = {}
+            strings.forEach((string) => { // string === 'email=11'
+                let parts = string.split('=') // ['email','11']
+                let key = parts[0]
+                let value = parts[1]
+                hash[key] = decodeURIComponent(value) // hash['email'] = '1'
+            })
+            let { email, password } = hash
+            if(email.indexOf('@') === -1){
+                response.statusCode = 400
+                response.setHeader('Content-Type', 'application/json; charset=utf-8')
+                response.write(`{
+                    "errors": {
+                        "email": "invalid"
+                    }
+                }`)
+            } else {
+                let users = fs.readFileSync('./db/users', 'utf8')
+                try{
+                    users = JSON.parse(users) // 将 JSON 字符串解析为对象,才能push
+                }catch(exception){
+                    users = []
+                }
+                let found
+                for( i = 0 ; i < users.length ; i++ ) {
+                    if (users[i].email === email && users[i].password === password) {
+                        found = true
+                        break;
+                    }
+                }
+                if(found) {
+                    response.statusCode = 200
+                }else{
+                    response.statusCode = 401
+                    response.setHeader('Content-Type', 'application/json; charset=utf-8')
+                    response.write(`{
+                        "errors": {
+                            "password": "unMatch"
+                        }
+                    }`)
+                } 
+                response.end()
+            }   
+        })
+    } //注意这里是 /main.js 而不是 ./main.js，因为HTTP请求永远是绝对路径
     else if (path === '/main.js') {
         let string = fs.readFileSync('./main.js', 'utf8')
         response.statusCode = 200

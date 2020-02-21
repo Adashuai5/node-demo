35301904088514b00264006a1a0b7c34014f0410
adashuai5 <ad873606339@gmail.com>
2020-02-21 17:12:15 +0800

    基本实现一个静态服务器

--- node-server/index.ts
+++ node-server/index.ts
@@ -9,30 +9,37 @@ const publicDir = p.resolve(__dirname, "public");
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   const { method, url: path, headers } = request;
-  const { pathname, search } = url.parse(path);
-  switch (pathname) {
-    case "/index.html":
-      response.setHeader("Content-Type", "text/html;charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/style.css":
-      response.setHeader("Content-Type", "text/css;charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "style.css"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/main.js":
-      response.setHeader("Content-Type", "text/javascript;charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "main.js"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
+
+  if (method !== "GET") {
+    response.statusCode = 405;
+    response.end("该页面为静态页面，不支持此操作！");
+    return;
   }
+
+  const { pathname, search } = url.parse(path);
+  let filename = pathname.slice(1);
+  if (filename === "") {
+    filename = "index.html";
+  }
+  fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+    if (error) {
+      if (error.errno === -4058) {
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, "404.html"), (error, data) => {
+          response.end(data);
+        });
+      } else if (error.errno === -4068) {
+        response.statusCode = 403;
+        response.end("您未获授权，无法查看此网页。");
+      } else {
+        response.statusCode = 500;
+        response.end("服务器繁忙");
+      }
+    } else {
+      response.setHeader("Cache-Control", "public, max-age=31536000");
+      response.end(data);
+    }
+  });
 });
 
 server.listen(8888, () => {

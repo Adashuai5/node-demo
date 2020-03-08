e6d129d54d0207b99c63ba0718e5b3c16fb522e1
adashuai5 <ad873606339@gmail.com>
2020-03-08 17:10:40 +0800

    use TypeScript

---
+++ express-demo-1/routes/users.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from "express";
+const express = require("express");
+const router = express.Router();
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+} as RequestHandler);
+router.get("/xxx", function(req, res, next) {
+  res.send("xxx respond with a resource");
+} as RequestHandler);
+
+module.exports = router;
